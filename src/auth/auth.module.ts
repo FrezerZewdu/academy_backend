e57efe843c17a464206f35b9ce3dfed3a878dc0f
Frezer <frezerzewdu@gmail.com>
2023-12-27 20:06:29 +0400

    bug fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthControllor } from './auth.controller';
+import { JwtStrategy } from './strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService],
+  imports: [JwtModule.register({})],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthControllor],
 })
 export class AuthModule {}
