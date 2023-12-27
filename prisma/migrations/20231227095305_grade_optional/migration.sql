5f6f8eeb7feb1eac01471782cdab7c79af881fa3
Frezer <frezerzewdu@gmail.com>
2023-12-27 14:44:56 +0400

    grade made optional

---
+++ prisma/migrations/20231227095305_grade_optional/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX `Billing_companyId_fkey` ON `billing`;
+
+-- DropIndex
+DROP INDEX `Module_Taken_moduleId_fkey` ON `module_taken`;
+
+-- DropIndex
+DROP INDEX `User_parentCompanyId_fkey` ON `user`;
+
+-- AlterTable
+ALTER TABLE `module_taken` MODIFY `grade` DOUBLE NULL;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_parentCompanyId_fkey` FOREIGN KEY (`parentCompanyId`) REFERENCES `Company`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Module_Taken` ADD CONSTRAINT `Module_Taken_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Module_Taken` ADD CONSTRAINT `Module_Taken_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `Module`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Billing` ADD CONSTRAINT `Billing_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
