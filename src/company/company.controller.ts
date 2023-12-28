bb212cc972e7b3d8bc3c11256c1213906fcb91ad
Frezer <frezerzewdu@gmail.com>
2023-12-28 15:52:50 +0400

    params error handling added

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -7,13 +7,19 @@ import {
   Post,
   Put,
   Query,
+  UnprocessableEntityException,
   UseGuards,
 } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { JwtGuard, RolesGuard } from 'src/auth/guard';
 import { HasRoles } from 'src/auth/decorator';
 import { Roles } from 'src/auth/dto/enums';
-import { createBillsDto, createCompanyDto, editCompanyDto } from './dto';
+import {
+  createBillsDto,
+  createCompanyDto,
+  editCompanyDto,
+  statusDto,
+} from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('company')
@@ -25,6 +31,19 @@ export class CompanyController {
   @HasRoles(Roles.admin, Roles.supervisor, Roles.registrar)
   fetchAllCompanies(@Query('search') search?: string) {
     return this.companyService.fetchAllCompanies({ search });
+  }
+
+  @Get(':companyId')
+  @UseGuards(RolesGuard)
+  @HasRoles(Roles.admin, Roles.supervisor)
+  fetchCompanyDetailById(@Param('companyId') companyId: string) {
+    const companyIdNumber = parseInt(companyId);
+    if (Number.isNaN(companyIdNumber)) {
+      throw new UnprocessableEntityException(
+        'The company ID passed is not a number',
+      );
+    }
+    return this.companyService.companyDetailById(companyIdNumber);
   }
 
   @Post()
@@ -45,7 +64,13 @@ export class CompanyController {
   @UseGuards(RolesGuard)
   @HasRoles(Roles.admin)
   deleteCompany(@Param('companyId') companyId: string) {
-    return this.companyService.deleteCompany(parseInt(companyId));
+    const companyIdNumber = parseInt(companyId);
+    if (Number.isNaN(companyIdNumber)) {
+      throw new UnprocessableEntityException(
+        'The company ID passed is not a number',
+      );
+    }
+    return this.companyService.deleteCompany(companyIdNumber);
   }
 
   @Post('bill')
@@ -58,7 +83,19 @@ export class CompanyController {
   @Put('bill/status/:billId')
   @UseGuards(RolesGuard)
   @HasRoles(Roles.admin, Roles.supervisor)
-  updateBillStatus(@Param('billId') billId: string, @Body() status: string) {
-    return this.companyService.updateBillStatus(parseInt(billId), status);
+  updateBillStatus(
+    @Param('billId') billId: string,
+    @Body() statusBody: statusDto,
+  ) {
+    const billIdNumber = parseInt(billId);
+    if (Number.isNaN(billIdNumber)) {
+      throw new UnprocessableEntityException(
+        'The Bill ID passed is not a number',
+      );
+    }
+    return this.companyService.updateBillStatus(
+      billIdNumber,
+      statusBody.status,
+    );
   }
 }
